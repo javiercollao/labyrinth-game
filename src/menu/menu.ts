d9a591cc1e8e7d69eb00b895dd57f81befdd1c03
Javier Collao <javier.collao@usach.cl>
2023-01-03 03:02:36 -0300

    menu avance

--- src/menu/menu.ts
+++ src/menu/menu.ts
@@ -1,16 +1,52 @@
+import PlayGame from "~/models/play-game"
 
+export default class Menu extends Phaser.Scene {
 
-
-export default class Menu extends Phaser.Scene {  
+    isIntro : Boolean;
     
     constructor() {
         super({
           key :'Menu'
         })
+        this.isIntro = true
+    }
+    
+    public setIsIntro (v : Boolean) {
+        this.isIntro = v;
+    }
+    
+    public create() : void {
+        this.isIntro? this.intro() : this.end()
     }
 
-    create() : void { 
-        console.log("Hola mundo")
+    public intro() : void{
+
+        this.add.image(0,0, 'menu-intro').setOrigin(0).setDepth(1)
+        this.button()
     }
+
+    public end() : void{
+        this.add.image(0,0, 'menu-end').setOrigin(0).setDepth(2)
+        this.button()
+    }
+
+    public button() : void{
+        let button = this.createButton()
+        button.setInteractive().on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () => {
+            //this.isIntro? this.setIsIntro(false): this.setIsIntro(true)
+            console.log("hoals")
+        })
+    }
+
+    public createButton(): Phaser.GameObjects.Image{
+        if(this.isIntro){
+            let buttonStart = this.add.image(330,289,'btn-menu-intro').setOrigin(0).setDepth(1)
+            return buttonStart
+        }
+        let buttonEnd = this.add.image(20,289,'btn-menu-end').setOrigin(0).setDepth(2)
+        return buttonEnd
+    }
+
+
 
 }
--- src/models/boot-game.ts
+++ src/models/boot-game.ts
@@ -2,6 +2,10 @@ import 'phaser';
 import frames from '../assets/sprites/sprites.json'
 import sprites from './../assets/sprites/sprites2.png'
 import main from './../assets/sprites/capa1.png'
+import menuIntroBackground from './../assets/sprites/menu-intro.png'
+import menuEndBackground  from './../assets/sprites/menu-end.png'
+import menuIntroButton  from './../assets/sprites/btn-menu.png'
+import menuEndButton from './../assets/sprites/btn-menu-end.png'
 import levels from '../assets/grid/levels.json'; 
 import ILoaderPhaser from './interfaces/phaserInterfaces/phaser.preloader.interface';
  
@@ -18,6 +22,10 @@ export default class BootGame extends Phaser.Scene implements ILoaderPhaser{
     this.load.atlas("sprites", sprites, frames) 
     this.load.image('mainGame', main)
     this.load.image('levelTiles', sprites)
+    this.load.image('menu-intro', menuIntroBackground )
+    this.load.image('menu-end', menuEndBackground )
+    this.load.image('btn-menu-intro', menuIntroButton )
+    this.load.image('btn-menu-end', menuEndButton )
   }
     
   create() : void { 
