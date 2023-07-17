cb1148d99a3f94173d6b76d0d3f9fad9c007e0cc
Javier Collao <javier.collao@usach.cl>
2023-07-17 03:02:29 -0400

    Add import new sprites

--- src/game/BootScene.ts
+++ src/game/BootScene.ts
@@ -3,6 +3,9 @@ import frames from '../assets/sprites/sprites.json'
 import sprites from './../assets/sprites/sprites2.png'
 import main from './../assets/sprites/capa1.png'
 import levels from '../assets/grid/levels.json'; 
+import btnReset from '../assets/sprites/reset-btn.png'
+import backLife from '../assets/sprites/back-life.png'
+import playerLife from '../assets/sprites/vida1.png'
 import menuIntroBackground from './../assets/sprites/menu-intro.png'
 import menuEndBackground  from './../assets/sprites/menu-end.png'
 import menuIntroButton  from './../assets/sprites/btn-menu.png'
@@ -22,6 +25,9 @@ export default class BootScene extends Scene{
       this.load.image('mainGame', main)
       this.load.image('levelTiles', sprites)
       this.load.image('menu-intro', menuIntroBackground )
+      this.load.image('btn-reset', btnReset)
+      this.load.image('back-life', backLife )
+      this.load.image('player-life', playerLife)
       this.load.image('menu-end', menuEndBackground )
       this.load.image('btn-menu-intro', menuIntroButton )
       this.load.image('btn-menu-end', menuEndButton )
